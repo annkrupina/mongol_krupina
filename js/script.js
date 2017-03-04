eee5525f5d53267d7c281b2313b8859647c3bc66
annkrupina <annkrupina1406@gmail.com>
2017-03-04 22:02:12 +0300

    result2

--- js/script.js
+++ js/script.js
@@ -2,30 +2,14 @@ $(document).ready(function(){
 	//$(".item-1").prev().hide();
 	$("li").on("click",function(){
 		console.log($(this).text());
-	});
-	$(".owl-carousel").owlCarousel({
-		loop: true,
-		nav: true,
-        navText: ["<i class='owl-prev'>","<i class='owl-next'>"],
-		navText: ["<img src='img/gallery-arrow.png'>","<img src='img/gallery-arrow2.png'>"],
-        responsive : {
-    
-    0 : {
-        items:1,
-    },
-    320: {
-    	items:2
-    },
+	});		
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        nav: true,
+        items: 4,
+        navText: ["<img src='img/gallery-arrow.png'>","<img src='img/gallery-arrow2.png'>"],
+    });
 
-    480 : {
-        items:3
-    },
-    // breakpoint from 768 up
-    768 : {
-        items:4
-    }
-}
-	});
     $(function() {
   $('select').selectric();
 });
