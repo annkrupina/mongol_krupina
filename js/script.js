f7ee0b4ad66cfda507fa7d41b15db2d12f3a02d9
annkrupina <annkrupina1406@gmail.com>
2017-03-05 03:51:32 +0300

    result3

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
 $(document).ready(function(){ 
-	//$(".item-1").prev().hide();
-	$("li").on("click",function(){
-		console.log($(this).text());
-	});		
-    $(".owl-carousel").owlCarousel({
+    $(".owl-carousel").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4
+});
+
+	
+   */ $(".owl-carousel").owlCarousel({
         loop: true,
         nav: true,
         items: 4,
         navText: ["<img src='img/gallery-arrow.png'>","<img src='img/gallery-arrow2.png'>"],
-    });
+    });*/
+
 
     $(function() {
   $('select').selectric();
 });
-    (function($) {
+    
 $(function() {
 
   $('#up').click(function() {
     $('html, body').animate({scrollTop: 0},500);
     return false;
   })
+})
 
-})
-})
 });
